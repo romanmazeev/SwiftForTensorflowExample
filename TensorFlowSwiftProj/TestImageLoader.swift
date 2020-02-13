5aeb7a57c56ba15e47b0fb4582345aa2ffd89299
Roman Mazeev <r.mazeev@redmadrobot.com>
2020-02-13 12:54:43 +0300

    Add `TestImageLoader` helper to recognize image

---
+++ TensorFlowSwiftProj/TestImageLoader.swift
@@ -0,0 +1,42 @@
+//
+//  TestImageLoader.swift
+//  TensorFlowSwiftProj
+//
+//  Created by Roman Mazeev on 05.01.2020.
+//  Copyright © 2020 Roman Mazeev. All rights reserved.
+//
+
+import Cocoa
+import TensorFlow
+
+class TestImageLoader {
+    func readDigit(filePath: String) -> Tensor<Float> {
+        guard let image = NSImage(byReferencingFile: filePath) else { fatalError("Can`t load test image") }
+        var proposedRect = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
+        guard let pixelValues = image.cgImage(forProposedRect: &proposedRect, context: nil, hints: nil)?.pixelValues else {
+            fatalError("Cant convert image to CGImage")
+        }
+
+        let scalars = pixelValues.map { Float(Int($0)) }
+
+        return Tensor(shape: [1, 1, Int(image.size.height), Int(image.size.width)], scalars: scalars)
+            .transposed(permutation: [0, 2, 3, 1]) / 255 // NHWC
+    }
+}
+
+extension CGImage {
+    var pixelValues: [UInt8] {
+        var pixelValues = [UInt8](repeating: 0, count: height * bytesPerRow)
+
+        let contextRef = CGContext(data: &pixelValues,
+                                   width: width,
+                                   height: height,
+                                   bitsPerComponent: bitsPerComponent,
+                                   bytesPerRow: bytesPerRow,
+                                   space: CGColorSpaceCreateDeviceGray(),
+                                   bitmapInfo: 0)
+        contextRef?.draw(self, in: CGRect(x: 0, y: 0, width: width, height: height))
+
+        return pixelValues
+    }
+}
